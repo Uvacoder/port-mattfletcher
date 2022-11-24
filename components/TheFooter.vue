79c58568a492c471690bf426d3b9e9672cf60f81
uvacoder <uva22940coder@gmail.com>
2022-11-24 17:48:54 -0500

    Update TheFooter.vue

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -7,7 +7,7 @@
             Built with <a href="https://v3.nuxtjs.org/" target="_blank">Nuxt 3</a> 💚 and <a href="https://tailwindcss.com/" target="_blank">TailwindCSS</a> 💙
           </p>
           <p class="text-sm font-semibold opacity-90">
-            Copyright &copy; Matt Fletcher {{ new Date().getFullYear() }}
+            Copyright &copy; uvacoder {{ new Date().getFullYear() }}
           </p>
         </div>
         <div class="col-span-12 sm:col-span-6 order-1 sm:order-2 text-center sm:text-right">
@@ -16,17 +16,13 @@
           </p>
           <div class="flex justify-center sm:justify-end items-center gap-4">
             <div>
-              <a href="mailto:mattfletcher94@outlook.com" class="hover:text-primary-500 active:text-primary-500 transition-colors" aria-label="Email">
+              <a href="mailto:uvacoder@proton.me" class="hover:text-primary-500 active:text-primary-500 transition-colors" aria-label="Email">
                 <IconMail class="w-6 h-6" />
               </a>
             </div>
+            
             <div class="">
-              <a href="https://instagram.com/mattfletcher.dev" class="hover:text-primary-500 active:text-primary-500 transition-colors" target="_blank" aria-label="Instagram">
-                <IconInstagram class="w-6 h-6" />
-              </a>
-            </div>
-            <div class="">
-              <a href="https://github.com/mattfletcher94" class="hover:text-primary-500 active:text-primary-500 transition-colors" target="_blank" aria-label="Github">
+              <a href="https://github.com/uvacoder" class="hover:text-primary-500 active:text-primary-500 transition-colors" target="_blank" aria-label="Github">
                 <IconGithub class="w-6 h-6" />
               </a>
             </div>
